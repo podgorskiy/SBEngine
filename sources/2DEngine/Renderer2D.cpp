620b18b0069b4321c46fe7896628e5cfa2bcca19
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-07-21 22:17:39 -0400

    Mesher

--- sources/2DEngine/Renderer2D.cpp
+++ sources/2DEngine/Renderer2D.cpp
@@ -100,114 +100,13 @@ void Renderer2D::SetUp(View view_box)
 }
 
 
-void Renderer2D::PrimReserve(int idx_count, int vtx_count)
-{
-	m_vertexArray.resize(m_vertexArray.size() + vtx_count);
-	m_indexArray.resize(m_indexArray.size() + idx_count);
-	_index_write_ptr = &m_indexArray[m_indexArray.size() - idx_count];
-	_vertex_write_ptr = &m_vertexArray[m_vertexArray.size() - vtx_count];
-}
-
-
-void Renderer2D::PrimReset()
-{
-	m_vertexArray.resize(0);
-	m_indexArray.resize(0);
-	_current_index = 0;
-	_index_write_ptr = &m_indexArray[0];
-	_vertex_write_ptr = &m_vertexArray[0];
-}
-
-
-void Renderer2D::PrimRect(const glm::vec2& a, const glm::vec2& c, const glm::vec2& uv_a, const glm::vec2& uv_c, color col)
-{
-	PrimReserve(6, 4);
-    glm::vec2 b(c.x, a.y), d(a.x, c.y), uv_b(uv_c.x, uv_a.y), uv_d(uv_a.x, uv_c.y);
-    auto idx = _current_index;
-    _index_write_ptr[0] = idx; _index_write_ptr[1] = idx+1; _index_write_ptr[2] = idx+2;
-    _index_write_ptr[3] = idx; _index_write_ptr[4] = idx+2; _index_write_ptr[5] = idx+3;
-    _vertex_write_ptr[0].pos = a; _vertex_write_ptr[0].uv = uv_a; _vertex_write_ptr[0].col = col;
-    _vertex_write_ptr[1].pos = b; _vertex_write_ptr[1].uv = uv_b; _vertex_write_ptr[1].col = col;
-    _vertex_write_ptr[2].pos = c; _vertex_write_ptr[2].uv = uv_c; _vertex_write_ptr[2].col = col;
-    _vertex_write_ptr[3].pos = d; _vertex_write_ptr[3].uv = uv_d; _vertex_write_ptr[3].col = col;
-    _vertex_write_ptr += 4;
-    _current_index += 4;
-    _index_write_ptr += 6;
-}
-
-void Renderer2D::PrimRectRounded(const glm::vec2& a, const glm::vec2& c, const glm::vec4& radius, color col)
-{
-	m_path.PathClear();
-	m_path.PathRect(a, c, radius);
-	PrimConvexPolyFilled(m_path.Ptr(), m_path.Count(), col);
-}
-
-void Renderer2D::PrimConvexPolyFilled(const glm::vec2* points, int count, color col)
-{
-    if (count < 3)
-        return;
-
-	const float anti_aliasing = 1.0f;
-	const color col_trans = color(glm::vec<3, uint8_t>(col), 0);
-	const int idx_count = glm::max(0, (count - 2) * 3) + count * 6;
-	const int vtx_count = count * 2;
-	PrimReserve(idx_count, vtx_count);
-
-	for (int i = 2; i < count; i++)
-	{
-		_index_write_ptr[0] = _current_index;
-		_index_write_ptr[1] = _current_index + 2 * i - 2;
-		_index_write_ptr[2] = _current_index + 2 * i - 0;
-		_index_write_ptr += 3;
-	}
-
-	glm::vec2 p1 = points[count - 2];
-	glm::vec2 p2 = points[count - 1];
-	glm::vec2 n1 = glm::normalize(p2 - p1);
-	n1 = glm::vec2(n1.y, -n1.x);
-	for (int i0 = count - 2, i1 = count - 1, i2 = 0; i2 < count; i0 = i1, i1 = i2++)
-	{
-		p1 = p2;
-		p2 = points[i2];
-
-		glm::vec2 n0 = n1;
-		n1 = glm::normalize(p2 - p1);
-		n1 = glm::vec2(n1.y, -n1.x);
-		float dm_x = (n0.x + n1.x) * 0.5f;
-		float dm_y = (n0.y + n1.y) * 0.5f;
-		float k = 0.5f * anti_aliasing / std::max(dm_x * dm_x + dm_y * dm_y, 0.5f);
-		dm_x *= k;
-		dm_y *= k;
-
-		_vertex_write_ptr[0].pos.x = (p1.x - dm_x);
-		_vertex_write_ptr[0].pos.y = (p1.y - dm_y);
-		_vertex_write_ptr[0].uv = glm::vec2(0.0, 0.0);
-		_vertex_write_ptr[0].col = col;
-		_vertex_write_ptr[1].pos.x = (p1.x + dm_x);
-		_vertex_write_ptr[1].pos.y = (p1.y + dm_y);
-		_vertex_write_ptr[1].uv = glm::vec2(0.0, 0.0);
-		_vertex_write_ptr[1].col = col_trans;
-		_vertex_write_ptr += 2;
-
-		_index_write_ptr[0] = _current_index + 2 * i1 + 0;
-		_index_write_ptr[1] = _current_index + 2 * i0 + 0;
-		_index_write_ptr[2] = _current_index + 2 * i0 + 1;
-		_index_write_ptr[3] = _current_index + 2 * i0 + 1;
-		_index_write_ptr[4] = _current_index + 2 * i1 + 1;
-		_index_write_ptr[5] = _current_index + 2 * i1 + 0;
-		_index_write_ptr += 6;
-	}
-	_current_index += vtx_count;
-}
-
-
 void Renderer2D::Draw()
 {
 	auto command_queue =  m_encoder.GetCommandQueue();
 
 	command_queue.Write(C_End);
 	command_queue.Seek(0);
-	PrimReset();
+	m_mesher.PrimReset();
 
 	bgfx::TextureHandle tex;
 
@@ -233,11 +132,11 @@ void Renderer2D::Draw()
 
 				if (radius == glm::vec4(0))
 				{
-					PrimRect(rect.minp, rect.maxp, glm::vec2(0.0, 0.0), glm::vec2(0.0, 0.0), col);
+					m_mesher.PrimRect(rect.minp, rect.maxp, glm::vec2(0.0, 0.0), glm::vec2(0.0, 0.0), col);
 				}
 				else
 				{
-					PrimRectRounded(rect.minp, rect.maxp, radius, col);
+					m_mesher.PrimRectRounded(rect.minp, rect.maxp, radius, col);
 				}
 			}
 			need_flush = true;
@@ -255,7 +154,7 @@ void Renderer2D::Draw()
 
 				if (radius == glm::vec4(0))
 				{
-					PrimRect(rect.minp, rect.maxp, uv.minp, uv.maxp, color(0));
+					m_mesher.PrimRect(rect.minp, rect.maxp, uv.minp, uv.maxp, color(0));
 				}
 				else
 				{
@@ -306,18 +205,18 @@ void Renderer2D::Draw()
 		{
 			case C_RectCol:
 			{
-				int num_vertex = m_vertexArray.size();
-				int num_index = m_indexArray.size();
+				int num_vertex = m_mesher.vcount();
+				int num_index = m_mesher.icount();
 				bgfx::TransientVertexBuffer tvb;
 				bgfx::TransientIndexBuffer tib;
 
 				bgfx::allocTransientVertexBuffer(&tvb, num_vertex, m_vertexSpec);
 				bgfx::allocTransientIndexBuffer(&tib, num_index);
 
-				memcpy(tvb.data, m_vertexArray.data(), num_vertex * sizeof(Vertex));
-				memcpy(tib.data, m_indexArray.data(), num_index * sizeof(uint16_t));
+				memcpy(tvb.data, m_mesher.vptr(), num_vertex * sizeof(Vertex));
+				memcpy(tib.data, m_mesher.iptr(), num_index * sizeof(uint16_t));
 
-				PrimReset();
+				m_mesher.PrimReset();
 
 				bgfx::setVertexBuffer(0, &tvb, 0, num_vertex);
 				bgfx::setIndexBuffer(&tib, 0, num_index);
@@ -332,18 +231,18 @@ void Renderer2D::Draw()
 			break;
 			case C_RectTex:
 			{
-				int num_vertex = m_vertexArray.size();
-				int num_index = m_indexArray.size();
+				int num_vertex = m_mesher.vcount();
+				int num_index = m_mesher.icount();
 				bgfx::TransientVertexBuffer tvb;
 				bgfx::TransientIndexBuffer tib;
 
 				bgfx::allocTransientVertexBuffer(&tvb, num_vertex, m_vertexSpec);
 				bgfx::allocTransientIndexBuffer(&tib, num_index);
 
-				memcpy(tvb.data, m_vertexArray.data(), num_vertex * sizeof(Vertex));
-				memcpy(tib.data, m_indexArray.data(), num_index * sizeof(uint16_t));
+				memcpy(tvb.data, m_mesher.vptr(), num_vertex * sizeof(Vertex));
+				memcpy(tib.data, m_mesher.iptr(), num_index * sizeof(uint16_t));
 
-				PrimReset();
+				m_mesher.PrimReset();
 
 				bgfx::setVertexBuffer(0, &tvb, 0, num_vertex);
 				bgfx::setIndexBuffer(&tib, 0, num_index);
--- sources/2DEngine/Renderer2D.h
+++ sources/2DEngine/Renderer2D.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Encoder.h"
+#include "Mesher.h"
 #include "View.h"
-#include "Path.h"
 #include "Vertices.h"
 #include "Render/Shader.h"
 #include "utils/aabb.h"
@@ -28,26 +28,15 @@ namespace Render
 		void Draw();
 
 		bool m_gamma_correction;
-	private:
-		void PrimReserve(int idx_count, int vtx_count);
-		void PrimReset();
-		void PrimRect(const glm::vec2& a, const glm::vec2& c, const glm::vec2& uv_a, const glm::vec2& uv_c, color col);
-		void PrimRectRounded(const glm::vec2& a, const glm::vec2& c, const glm::vec4& radius, color col);
-		void PrimConvexPolyFilled(const glm::vec2* points, int count, color col);
 
+	private:
 		Scriber::Driver m_text_driver;
 
 		Encoder m_encoder;
-		Path m_path;
+		Mesher m_mesher;
 
 		bool scissoring_enabled = false;
 
-		std::vector<uint16_t> m_indexArray;
-		std::vector<Vertex> m_vertexArray;
-
-		uint16_t _current_index;
-		uint16_t* _index_write_ptr;
-		Vertex* _vertex_write_ptr;
 		glm::aabb2 current_sciscors;
 
 		Render::ProgramPtr m_programCol;
